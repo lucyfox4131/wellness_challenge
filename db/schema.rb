0f3ce2f348879af32deeee27d6b036df14685f3a
Lucy Fox <lucyreneefox@gmail.com>
2016-06-05 12:52:37 -0600

    happy path user authentication functionality All happy path user authentication functionality added. Users can create a new account, login and logout. Feature tests added for each of these actions but only currently for the happy path scenarios.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603203428) do
+ActiveRecord::Schema.define(version: 20160605180548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20160603203428) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "password_digest"
+    t.integer  "role"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
